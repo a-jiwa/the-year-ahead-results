897df6ddc42af95591f0d13a77cf191849ec7932
adnaanjiwa <adnaanjiwa@gmail.com>
2025-01-13 20:37:01 +0000

    Grafana

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,15 @@
-// src/logger.js
 const logToLoki = async (message, level = 'info') => {
+    // Split the token into parts
+    const tokenPart1 = '1039188';
+    const tokenPart2 = 'glc_eyJvIjoiMTI2MzA3OCIsIm4iOiJzdGFjay0xMDgxNDA5LWhsLXJlYWQtZmlyc3QiLCJrIjoieThDMUc2MTExM3U1Z0k2OFh4UE50RXpPIiwibSI6eyJyIjoicHJvZC1ldS13ZXN0LTIifX0=';
+
     try {
+        // Combine the token parts to construct the Authorization header
         await fetch('https://logs-prod-012.grafana.net/loki/api/v1/push', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: 'Basic ' + btoa('1039188:glc_eyJvIjoiMTI2MzA3OCIsIm4iOiJzdGFjay0xMDgxNDA5LWhsLXJlYWQtZmlyc3QiLCJrIjoieThDMUc2MTExM3U1Z0k2OFh4UE50RXpPIiwibSI6eyJyIjoicHJvZC1ldS13ZXN0LTIifX0='), // Replace YOUR_API_TOKEN
+                Authorization: 'Basic ' + btoa(`${tokenPart1}:${tokenPart2}`),
             },
             body: JSON.stringify({
                 streams: [
